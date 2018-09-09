e5a3741559328bdbf1f976f68eb35ae618eaf888
Insup Jung <31408300+jis1218@users.noreply.github.com>
2018-09-09 23:46:10 +0900

    commit

---
+++ MVVMPrac/ViewModels/DataViewModel.cs
@@ -0,0 +1,21 @@
+﻿using MVVMPrac.Models;
+using System;
+
+namespace MVVMPrac.ViewModels
+{
+    public class DataViewModel : NotificationBase<Data>
+    {
+        private Data _task;
+
+        public DataViewModel()
+        {
+            _task = new Data();
+        }
+
+        public string Name
+        {
+            get { return This.Name; }
+            set { SetProperty(This.Name, value, () => This.Name = value); }
+        }        
+    }
+}
---
+++ MVVMPrac/ViewModels/NotificationBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace MVVMPrac.ViewModels
+{
+    public class NotificationBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string property = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            RaisePropertyChanged(property);
+            return true;
+        }
+
+        protected void SetProperty<T>(T currentValue, T newValue, Action doSet, [CallerMemberName] string property = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(currentValue, newValue)) return;
+            doSet.Invoke();
+            RaisePropertyChanged(property);
+        }
+
+        private void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+            //if (PropertyChanged != null)
+            //{
+            //    PropertyChanged(this, new PropertyChangedEventArgs(property));
+            //}
+        }
+    }
+
+    public class NotificationBase<T> : NotificationBase where T : class, new()
+    {
+        protected readonly T This;
+
+        public static implicit operator T(NotificationBase<T> thing) { return thing.This; }
+
+        protected NotificationBase(T thing = null)
+        {
+            This = thing ?? new T();
+        }
+    }
+}
